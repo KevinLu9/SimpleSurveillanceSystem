82b3489da8af956e1f21b0dcab8cf27812f038f9
kluu0012 <kevin.lu902@gmail.com>
2022-12-08 03:44:27 +1100

    Added camera disconnection error handling
    System now checks to reconnect the camera.
    System alerts the user by outputting text to the camera feed

--- camera.py
+++ camera.py
@@ -1,5 +1,11 @@
+## Simple Surveillance System ##
+## Author: Kevin Lu ##
+## Start Date: 08/12/2022 ##
+
 import cv2
+import time
 import threading
+
 
 class CameraCapture(object):
     def __init__(self, index):
@@ -10,6 +16,9 @@ class CameraCapture(object):
         self.width = self.cam.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.index = index
         threading.Thread(target=self.update, args=()).start()
+        self.status = True
+        self.MAX_TIMEOUT_SECONDS = 20
+        self.timeout = 0
 
         
     def __str__(self):
@@ -20,18 +29,42 @@ class CameraCapture(object):
 
 
     def get_frame(self):
-        image = self.frame
-        return image
-        # Output as JPG for webapp video feed
-        #_, jpeg = cv2.imencode('.jpg', image)
-        #return jpeg.tobytes()
+        if self.status:
+            image = self.frame
+            return image
+            #_, jpeg = cv2.imencode('.jpg', image)
+            #return jpeg.tobytes()
+        else:
+            # Handle Timeout countdown
+            count = self.MAX_TIMEOUT_SECONDS -(time.time() - self.timeout)
+            if time.time() - self.timeout >= self.MAX_TIMEOUT_SECONDS:
+                self.__del__()   
+            #
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            self.cam = cv2.VideoCapture(self.index)
+            image = self.frame.copy()
+            return cv2.putText(image,
+                               f"No Camera Signal, Timeout in: {round(count, 2)}s",
+                               (4, int(self.height//2)),
+                               font,
+                               1,
+                               (0, 0, 255),
+                               1,
+                               cv2.LINE_AA)
 
 
     def update(self):
         while True:
-            self.grabbed, self.frame = self.cam.read()
- 
-
+            g, f = self.cam.read()
+            if f is not None:
+                (self.grabbed, self.frame) = (g, f)
+                self.status = True
+            else:
+                if self.status:
+                    self.timeout = time.time()
+                self.status = False
+                
+                
     def __del__(self):
         self.cam.release()
 
@@ -41,7 +74,8 @@ if __name__ == "__main__":
     print(cam)
     
     while True:
-        cv2.imshow(f"Camera {cam.index}", cam.get_frame())
+        aframe = cam.get_frame()
+        cv2.imshow(f"Camera {cam.index}", aframe)
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             del cam
